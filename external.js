cc94d6fe7cef74d858acaf7b8027ff51233545e7
Hannah Sturm <44344697+hcsturm@users.noreply.github.com>
2018-12-02 18:47:54 -0600

    FormCode

---
+++ external.js
@@ -0,0 +1,12 @@
+function myFunction(){
+	var contact = [];
+	var UserData = document.getElementById("addressform").value;
+	let name = document.getElementById("yourname").value;
+	let phone = document.getElementById("phone").value;
+	let email = document.getElementById("email").value;
+	
+	window.alert("Hi, " + name +  "! Your information has been stored to the customer database!");
+	
+}
+
+
---
+++ rectangleform.js
@@ -0,0 +1,27 @@
+//function calculateArea() {
+//	var width = document.getElementById("width").value;
+//	var height = document.getElementById("height").value;
+//	var area = wocument.getElementById("area");
+//	document.getElementById("area").value = area;
+//}
+
+//function calculateArea() {
+	//defining varaibles
+//	var x, width, length, area;
+	
+	//retreive information put in calculateArea form and put into x variable
+	//x = document.getElementById("calculateArea");
+	
+	//width = document.getElementById("width").value;
+	//length = document.getElementById("length").value;
+	//area = width * length;
+	
+	//document.getElementById("1").value = area;
+//}
+
+function solveArea() {
+	var width = document.getElementById("width").value;
+	var length = document.getElementById("length").value;
+	var div1 = width * length;
+	document.getElementById("div1").value = div1;
+}
